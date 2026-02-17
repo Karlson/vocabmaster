fe09017db629b0cce768754b347c9da5acaddc20
Vadim <vadim@mac.local>
2026-02-17 18:26:33 +0200

    VocabMaster PWA

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'vocabmaster-v1';
+const URLS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => 
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
